d53f7008758a3f655bfaaffedb14eb6c036e29c6
RamazanMamyrbek <rama.mamirbek@gmail.com>
2023-07-26 23:15:22 +0600

    s

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int res = number/1000 + (number-(number/1000)*1000)/100 + (number-(number/100)*100)/10 + (number-(number/10)*10);
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        StringBuilder stringBuilder = new StringBuilder(number+"");
+        System.out.println(stringBuilder.reverse());
     }
 }
